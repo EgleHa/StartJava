caab8d973fd1f1e75021c04b3b8dc733108dfcea
user <user@users-MBP.lan>
2022-03-02 13:14:44 +0200

    Initial commit

---
+++ src/com/company/Conditionals.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Conditionals {
+    public static void main(String[] args) {
+        int number = 15;
+        if (number > 9 && number <=14){
+            System.out.println("the value is:" + number);
+        } else {
+            System.out.println("nothing");
+        }
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        int number = 4;
+        int value = 6;
+        System.out.println("Addition of number and value is: " + (number + value));
+        System.out.println("Substraction of number and value is: " + (number - value));
+        System.out.println("Multiplication of number and value is: " + (number * value));
+        System.out.println("Division of number and value is: " + (number / value));
+        System.out.println("Hello world!");
+
+        int age = 41;
+        String gender = "Female";
+        float height = 1.61f;
+        int weight = 53;
+
+        System.out.println(5 == 9);
+        System.out.println(9 <= 19);
+        System.out.println(5 < 8 && 8 > 3);
+        System.out.println(5 < 8 || 8 > 3);
+
+        System.out.println(6 < 9 && 9 > 2);
+        System.out.println(5 > 9 || 8 < 3);
+        System.out.println(8 == 2 && 7 < 6);
+
+
+    }
+}
